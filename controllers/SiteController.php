13a1e0d95aa0407bdbfeb8497a85e4addc729afd
Vasiliy <kkolyasev1228@gmail.com>
2022-03-09 12:15:15 +0400

    start tg bot

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -2,16 +2,13 @@
 
 namespace app\controllers;
 
-use Yii;
-use yii\filters\AccessControl;
 use yii\web\Controller;
-use yii\web\Response;
-use yii\filters\VerbFilter;
-use app\models\LoginForm;
-use app\models\ContactForm;
 
 class SiteController extends Controller
 {
+    public $enableCsrfValidation = false;
+    private $url = 'https://api.telegram.org/bot5176485026:AAErX78KRuBl0cAOdByWdLgudAdkkPQ5od0/';
+    private $offset = '';
 
     public function actions()
     {
@@ -23,7 +20,23 @@ class SiteController extends Controller
     }
     public function actionIndex()
     {
-        return $this->render('index');
+        $updates = $this->sendRequest('getUpdates', ['offset' => 476511670]);
+        foreach ($updates['result'] as $update) {
+            $chat_id = $update['message']['chat']['id'];
+            var_dump($chat_id . PHP_EOL);
+            $this->sendRequest('sendMessage', ['chat_id' => $chat_id, 'text' => 'Yaa']);
+        }
+        var_dump($updates);
+        //return $this->render('index');
     }
 
+    private function sendRequest($method, $params = [])
+    {
+        if (!empty($params)) {
+            $url = $this->url . $method . '?' . http_build_query($params);
+        } else {
+            $url = $this->url . $method;
+        }
+        return json_decode(file_get_contents($url), JSON_OBJECT_AS_ARRAY);
+    }
 }
